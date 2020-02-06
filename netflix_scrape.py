b3746f8cec7ce5e84c37e18bb6e34addd2b884ff
Anirudh Kachroo <anirudh.kac@yahoo.com>
2020-02-06 12:08:28 +0530

    scrape netflix originals titles

---
+++ netflix_scrape.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+base_url ="https://www.netflix.com/in/browse/genre/839338"
+nf = requests.get(base_url)
+#print(nf.text)
+soup = BeautifulSoup(nf.text,"html.parser")
+horizontal_row_items=soup.find_all("img",class_="nm-collections-title-img")
+with open("netflix_titles.csv","w") as file:
+    csv_writer = writer(file)
+    csv_writer.writerow(["Name","ID"])
+    for row_item in horizontal_row_items:
+        csv_writer.writerow([row_item["alt"],row_item["data-title-id"]])
+        
+
+
